919f52e54f3c9f000c99489068ffd87211fb4be0
zapicojunredexter <zapicojunredexter@yahoo.com>
2019-04-02 17:48:07 +0800

    added user routes

--- functions/src/routes/userRoutes.js
+++ functions/src/routes/userRoutes.js
@@ -1,17 +1,129 @@
+const Firestore = require('@google-cloud/firestore');
 const express = require("express");
 const app = express();
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+const COLLECTION_NAME = 'Users';
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
 
 app.get("/",
     (req, res) => {
-        res.send('HELLO WORLD LISzxcT');
+        const userRef = db.collection(COLLECTION_NAME);
+        userRef
+            .get()
+            .then(doc => {
+                if (doc.empty) {
+                    return res.send([]);
+                }
+                return res.status(200).send(doc.docs.map(obj => obj.data()));
+            })
+            .catch(error => {
+                return res.status(500).send(error);
+            });
     }
 );
-
 
 app.get("/:id",
     (req, res) => {
         const id = req.params.id;
-        res.send('HELLO WORLD'+id);
+        const userRef = db.collection(COLLECTION_NAME);
+        userRef
+            .doc(id)
+            .get()
+            .then(doc => {
+                if (!doc.exists) {
+                    return res.send([]);
+                }
+                return res.status(200).send(doc.data());
+            })
+            .catch(error => {
+                return res.status(500).send(error);
+            });
+    }
+);
+
+app.post("/",
+    async (req, res) => {
+        const { body } = req;
+        const {
+            id
+        } = body;
+        try {
+            const userRef = db.collection(COLLECTION_NAME);
+            const addedUser = await userRef.add(body);
+            const { id } = addedUser;
+
+            const editedUser = await userRef.doc(id).set({ id }, { merge: true });
+            
+            return res.status(200).send({ success : true, res: editedUser });
+        } catch (error) {
+            return res.status(500).send(error);
+        }
+        // const userRef = db.collection(COLLECTION_NAME);
+        // userRef
+        //     .add(body)
+        //     .then(doc => {
+        //         const { id } = doc;
+        //         return userRef
+        //             .doc(id)
+        //             .set({ id }, { merge: true })
+        //             .then(doc => {
+        //                 return res.status(200).send({ success : true });
+        //             })
+        //             .catch(error => {
+        //                 return res.status(500).send(error);
+        //             })
+        //     })
+        //     .catch(error => {
+        //         return res.status(500).send(error);
+        //     });
+    }
+);
+
+app.put("/",
+    (req, res) => {
+        const { body } = req;
+        const {
+            id
+        } = body;
+        const userRef = db.collection(COLLECTION_NAME);
+        userRef
+            .doc(id)
+            .set(body, { merge: true })
+            .then(doc => {
+                return res.status(200).send({ success : true });
+            })
+            .catch(error => {
+                return res.status(500).send(error);
+            });
+    }
+);
+
+app.delete("/",
+    (req, res) => {
+        const { body } = req;
+        const {
+            id
+        } = body;
+        const userRef = db.collection(COLLECTION_NAME);
+        userRef
+            .doc(id)
+            .set({ deleted : true }, { merge: true })
+            .then(doc => {
+                return res.status(200).send({ success : true });
+            })
+            .catch(error => {
+                return res.status(500).send(error);
+            });
+    }
+);
+
+app.get("/testzxc",
+    (req, res) => {
     }
 );
 
